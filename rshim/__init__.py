bad7bf863b171f9579277416e72ac2ee6c433960
Ryan <ryan.shim@gmial.com>
2016-10-11 13:47:19 -0700

    updated bootstrap files and added sub-sites

--- rshim/__init__.py
+++ rshim/__init__.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("main.html", title="Test Title", paragraph="This is a file transfer test; website in production!")
+    return render_template("main.html", title="Ryan Shim", paragraph="\nThis is a file transfer test; website in production!")
+
+@app.route('/projects/')
+def projects():
+    return render_template("project.html", title="Projects Completed", paragraph="This is the projects page; website in production", home="Return to homepage")
+
+@app.route('/contact/')
+def contact():
+    return render_template("contactForm.html", title="Contact Me", paragraph="This is the contact me page; website in production", home="Return to homepage")
 
 if __name__ == "__main__":
     app.run()
