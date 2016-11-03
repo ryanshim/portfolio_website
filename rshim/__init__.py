c903590812e2bcbf7c75346d2bb70bd4cd8a4878
Ryan <ryan.shim@gmial.com>
2016-11-03 14:19:09 -0700

    rendering bio more pythonic

--- rshim/__init__.py
+++ rshim/__init__.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     with app.open_resource('static/aboutMe.txt') as f:
-        aboutMe1 = f.readline()
-        aboutMe2 = f.readline()
-        aboutMe3 = f.readline()
-        aboutMe4 = f.readline()
-    return render_template("main.html", title="Ryan Shim", paragraph="Website in production!", bio1=aboutMe1, bio2=aboutMe2, bio3=aboutMe3, bio4=aboutMe4)
+        aboutMe = f.read().split('\n')
+    return render_template("main.html", title="Ryan Shim", paragraph="Website in production!", bio=aboutMe)
 
 @app.route('/projects/')
 def projects():
-    return render_template("project.html", title="Projects Completed", paragraph="This is the projects page; website in production", home="Return to homepage")
+    return render_template("project.html",
+                            title="Projects Completed",
+                            paragraph="This is the projects page; website in production",
+                            home="Return to homepage")
 
 @app.route('/contact/')
 def contact():
-    return render_template("contactForm.html", title="Contact Me", paragraph="This is the contact me page; website in production", home="Return to homepage")
+    return render_template("contactForm.html",
+                            title="Contact Me",
+                            paragraph="This is the contact me page; website in production",
+                            home="Return to homepage")
 
 if __name__ == "__main__":
     app.run()
