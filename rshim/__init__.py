e6a1688e4a07db397ca43c344a105d2b66526b1d
ryanshim <ryan.shim@gmail.com>
2017-01-08 18:27:30 -0800

    contact page working

--- rshim/__init__.py
+++ rshim/__init__.py
@@ -8,38 +8,42 @@ app.config.update(
         MAIL_SERVER='smtp.gmail.com',
         MAIL_PORT=465,
         MAIL_USE_SSL=True,
-        MAIL_USERNAME='yourEmailHere',
-        MAIL_PASSWORD='yourPassHere'
+        MAIL_USERNAME='emailHere',
+        MAIL_PASSWORD='passHere'
         )
 mail = Mail(app)
 
+# main page
 @app.route('/')
 def homepage():
     with app.open_resource('static/aboutMe.txt') as f:
         aboutMe = f.read().split('\n')
     return render_template("main.html",
-                            title="Ryan Shim",
-                            paragraph="Website in production!",
-                            bio=aboutMe,
-                            gitLink="https://github.com/ryanshim",
-                            gitLogo="static/images/GitHub-Mark-64px.png",
-                            linkedInLink="https://www.linkedin.com/in/ryan-shim-2a119a1a",
-                            linkedInLogo="static/images/linkedIn.png")
+            title="Ryan Shim",
+            paragraph="Website in production!",
+            bio=aboutMe,
+            gitLink="https://github.com/ryanshim",
+            gitLogo="static/images/GitHub-Mark-64px.png",
+            linkedInLink="https://www.linkedin.com/in/ryan-shim-2a119a1a",
+            linkedInLogo="static/images/linkedIn.png")
 
+# projects page
 @app.route('/projects/')
 def projects():
     return render_template("project.html",
-                            title="Projects",
-                            paragraph="This is the projects page; website in production",
-                            gitPrj1="https://github.com/ryanshim/portfolio_website/",
-                            gitPrj1Name="Portfolio Website")
+            title="Projects",
+            paragraph="This is the projects page; website in production",
+            gitPrj1="https://github.com/ryanshim/portfolio_website/",
+            gitPrj1Name="Portfolio Website")
 
+# contact page (form)
 @app.route('/contact/')
 def contact():
     return render_template("contactForm.html",
-                            title="Contact Me",
-                            paragraph="This is the contact me page; website in production")
+            title="Contact Me",
+            paragraph="This is the contact me page; website in production")
 
+# message sent confirmation page
 @app.route('/sendMail/', methods=["GET", "POST"])
 def handleRequests():
     userEmail = request.form["inputEmail"]
@@ -47,15 +51,18 @@ def handleRequests():
 
     if request.method == "POST":
         sendMail(userEmail, userText)
-        return render_template("result.html", userText=userText, userEmail=userEmail)
+        return render_template("result.html",
+                confirm="Message sent! I will get back to you shortly!",
+                backHome="Click here to go back to the homepage.")
     else:
-        return render_template("result.html", userText="F", userEmail="F")
+        return render_template("result.html",
+                confirm="Message failed to deliver")
 
 def sendMail(userEmail, userText):
     try:
-        msg = Message("New website contact message",
-                sender="sendEmailToYourself",
-                recipients=["sendEmailToYourself"])
+        msg = Message("New website contact message from: " + str(userEmail),
+                sender="sameEmailHere",
+                recipients=["sameEmailHere"])
         msg.body = userText
         mail.send(msg)
         return "Mail Sent"
