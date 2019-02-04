248e93fe748197521e0f2b87fbf42e8fdba000cd
Ryan <ryan.shim@gmail.com>
2019-02-04 11:16:16 -0800

    added fixed navbar

--- portfolio_website/static/js/main_animations.js
+++ portfolio_website/static/js/main_animations.js
@@ -1,3 +1,9 @@
+$("#main-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#trgt-main").offset().top
+    }, 1000);
+});
+
 $("#prj-btn").click(function() {
     $('html, body').animate({
         scrollTop: $("#trgt-projects").offset().top
